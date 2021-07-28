f67286c6db7bd289ca25c458b0f4ff5d8c6ef277
IlhunKIm <kimv777@naver.com>
2021-07-28 12:17:52 +0900

    Initial commit

---
+++ src/main/java/com/finalpjc/ott/controller/ArticleController.java
@@ -0,0 +1,57 @@
+package com.finalpjc.ott.controller;
+
+import com.finalpjc.ott.dto.ArticleRequestDto;
+import com.finalpjc.ott.model.Article;
+import com.finalpjc.ott.repository.ArticleRepository;
+import com.finalpjc.ott.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class ArticleController {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleService articleService;
+
+    @PostMapping("/user/article") //게시글 작성
+    public void createArticle(@RequestBody ArticleRequestDto requestDto) {
+        articleService.createArticle(requestDto);
+    }
+
+    @GetMapping("/user/all-article/{username}/{page}/{size}")//전체 게시글 조회
+    public Page<Article> getAllArticleWithSize(@PathVariable String username, @PathVariable int page, @PathVariable int size) {
+        Pageable pageable = PageRequest.of(page -1, size);
+        Page<Article> articleList = articleRepository.findAllByOrderByCreatedAtDesc(pageable);
+        return articleService.getPagedArticleList(articleList, username);
+    }
+
+    @GetMapping("/user/my-article/{username}/{page}/{size}")
+    public Page<Article> getArticleWithSize(@PathVariable String username, @PathVariable int page, @PathVariable int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        Page<Article> articleList = articleRepository.findAllByUsernameOrderByCreatedAtAsc(username, pageable);
+        return articleService.getPagedArticleList(articleList, username);
+    }
+
+    @GetMapping("/user/article/{articleId}")
+    public Article getArticleByArticleId(@PathVariable Long articleId) {
+        return articleService.getArticleByArticleId(articleId);
+    }
+
+    @PutMapping("/user/article/{articleId}")
+    public void updateArticle(@PathVariable Long articleId, @RequestBody ArticleRequestDto requestDto) {
+        articleService.updateArticle(articleId, requestDto);
+    }
+
+    @DeleteMapping("/user/article/{articleId}")
+    public void deleteArticle(@PathVariable Long articleId) {
+        articleService.deleteArticle(articleId);
+    }
+
+
+
+
+}
---
+++ src/main/java/com/finalpjc/ott/service/ArticleService.java
@@ -0,0 +1,78 @@
+package com.finalpjc.ott.service;
+
+import com.finalpjc.ott.dto.ArticleRequestDto;
+import com.finalpjc.ott.model.Article;
+import com.finalpjc.ott.model.Comment;
+import com.finalpjc.ott.repository.ArticleLikeItRepository;
+import com.finalpjc.ott.repository.ArticleRepository;
+import com.finalpjc.ott.repository.CommentRepository;
+import com.finalpjc.ott.repository.RecommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+    private final CommentRepository commentRepository;
+    private final RecommentRepository recommentRepository;
+    private final ArticleLikeItRepository articleLikeItRepository;
+
+   @Transactional
+    public void createArticle(ArticleRequestDto articleRequestDto) {
+       Article article = new Article(articleRequestDto);
+       articleRepository.save(article);
+   }
+
+   @Transactional
+    public void updateArticle(Long articleId, ArticleRequestDto articleRequestDto) {
+       Article article = articleRepository.findById(articleId).orElseThrow(
+               () -> new NullPointerException("ID not found")
+       );
+       article.articleUpdate(articleRequestDto);
+   }
+
+   public Page<Article> getPagedArticleList(Page<Article> articlelist, String username) {
+       for (Article value : articlelist) {
+           Long articleId = value.getId();
+           List<Comment> commentList = commentRepository.findAllByArticleId(articleId);
+           Long recommentCount = 0L;
+
+           for (Comment comment : commentList) {
+               recommentCount += recommentRepository.countByCommentId(comment.getId());
+           }
+
+           Long commentCount = commentRepository.countByArticleId(articleId);
+           Long likescount = articleLikeItRepository.countByArticleId(articleId);
+
+           value.addComentCount(commentCount + recommentCount);
+           value.addLikeItCount(likescount);
+
+           value.changeLikeItChecker(articleLikeItRepository.findByUsernameAndArticleId(username, articleId).isPresent());
+           articleLikeItRepository.findAllByArticleId(articleId).forEach(value::addLikeItUserList);
+       }
+       return articlelist;
+
+   }
+
+   @Transactional
+    public void deleteArticle(Long articleId){
+       articleRepository.deleteById(articleId);
+   }
+
+   public Article getArticleByArticleId(Long articleId) {
+       return articleRepository.findById(articleId).orElseThrow(
+               () -> new NullPointerException("articleId not found")
+       );
+   }
+
+
+
+
+
+}
