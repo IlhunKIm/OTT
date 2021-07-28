9720e7edf187ba773208c7dc23faf13549f764f4
IlhunKIm <kimv777@naver.com>
2021-07-28 11:05:00 +0900

    Initial commit

--- src/main/java/com/finalpjc/ott/repository/UserProfileRepository.java
+++ src/main/java/com/finalpjc/ott/repository/UserProfileRepository.java
@@ -1,10 +1,25 @@
 package com.finalpjc.ott.repository;
 
 import com.finalpjc.ott.model.UserProfile;
+import com.finalpjc.ott.repository.mapping.FriendObjectMappingFromUserProfile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
 
+    Optional<UserProfile> findByUsername(String username);
+
+    //    UserProfile findByUsername(String username);
+    FriendObjectMappingFromUserProfile getByUsername(String username);
+    List<UserProfile> findAllByUsername(String username);
+    List<FriendObjectMappingFromUserProfile> findAllByOrderByModifiedAtDesc();
+    Page<FriendObjectMappingFromUserProfile> findAllByOrderByCreatedAtDesc(Pageable pageable);
+    List<UserProfile> findAll();
+    List<FriendObjectMappingFromUserProfile> findAllByUsernameContaining(String username);
 
 
 }
