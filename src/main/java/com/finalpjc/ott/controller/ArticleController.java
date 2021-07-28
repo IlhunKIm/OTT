a6f8c176a7afb3f580a57c0ef7d74614ab415b40
IlhunKIm <kimv777@naver.com>
2021-07-28 12:32:13 +0900

    Initial commit

--- src/main/java/com/finalpjc/ott/controller/ArticleController.java
+++ src/main/java/com/finalpjc/ott/controller/ArticleController.java
@@ -51,7 +51,4 @@ public class ArticleController {
         articleService.deleteArticle(articleId);
     }
 
-
-
-
 }
--- src/main/java/com/finalpjc/ott/repository/ArticleLikeItRepository.java
+++ src/main/java/com/finalpjc/ott/repository/ArticleLikeItRepository.java
@@ -2,11 +2,12 @@ package com.finalpjc.ott.repository;
 
 import com.finalpjc.ott.model.ArticleLikeIt;
 import com.finalpjc.ott.repository.mapping.UsernameMapping;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ArticleLikeItRepository {
+public interface ArticleLikeItRepository extends JpaRepository<ArticleLikeIt, Long> {
     Long countByArticleId(Long articleId);
     void deleteByUsernameAndArticleId(String username, Long articleId);
     Optional<ArticleLikeIt> findByUsernameAndArticleId(String username, Long articleId);
